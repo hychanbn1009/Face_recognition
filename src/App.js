7a344f87fa8ebf1e9854059233658664ffe52c7f
Fishball Yuen <oxfishballxo@gmail.com>
2021-09-12 21:43:09 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import './App.css';
+import Particles from 'react-particles-js';
+import React,{ Component } from 'react';
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
+const app = new Clarifai.App({
+ apiKey: 'adfa083445024b1cb315bb88ac928381'
+});
+
+const particlesOptions={
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:"",
+      imageUrl:"",
+      box:{},
+    }
+  }
+
+  onInputChange=(event)=>{
+    this.setState({input: event.target.value});
+  }
+
+  calculateFaceLocation=(data)=>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col*width,
+      topRpw: clarifaiFace.top_row*height,
+      rightCol: width - (clarifaiFace.right_col*width),
+      bottomRow: height - (clarifaiFace.bottom_row*height),
+    }
+  }
+
+  displayFaceBox=(box)=>{
+    console.log(box);
+    this.setState({box: box});
+  }
+
+
+  onButtonSubmit=()=>{
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(      
+      response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err=> console.log(err));
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className="particles"
+                params={particlesOptions} />
+      <Navigation/>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   );
+  }
 }
 
 export default App;
